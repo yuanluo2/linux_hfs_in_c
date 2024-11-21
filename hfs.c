241e29f1d30d166c32a5aa7abfbc07eeebfdfdfb
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-11-21 16:13:37 +0800

    Update hfs.c

--- hfs.c
+++ hfs.c
@@ -1,5 +1,5 @@
 /*
-	@author yuanluo2
+    @author yuanluo2
  
     file server based on http protocol, written in ANSI C, only single file.
     this program is used to seek the file list of my server or upload files to my server.
